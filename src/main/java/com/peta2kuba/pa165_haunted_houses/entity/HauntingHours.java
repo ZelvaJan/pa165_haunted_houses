5b7e764f7ecbf7cb1eba5b89cd97933444b90a8e
DrakMelisek <petr.melicherik@seznam.cz>
2015-10-28 19:35:14 +0100

    Added hauntingHours class and modified Haunter with relation.

--- src/main/java/com/peta2kuba/pa165_haunted_houses/entity/HauntingHours.java
+++ src/main/java/com/peta2kuba/pa165_haunted_houses/entity/HauntingHours.java
@@ -1,43 +1,29 @@
 package com.peta2kuba.pa165_haunted_houses.entity;
 
+import java.sql.Time;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
-import java.sql.Time;
-import javax.persistence.ManyToOne;
 
 /**
  * @author petr.melicherik
  */
 @Entity
-public class Haunter {
+public class HauntingHours {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    @Column(nullable = false, unique = true)
-    private String name;
-
-    @ManyToOne(optional=false)
-    @NotNull
-    private HauntingHours hauntingHours;
+    @Column(nullable = false)
+    private Time from;
 
     @NotNull
     @Column(nullable = false)
-    private String description;
-
-    @NotNull
-    @Column(nullable = false)
-    private String hauntingReason;
-
-    @OneToMany
-    @Column(nullable = false)
-    private Ability ability;
+    private Time to;
 
 }
