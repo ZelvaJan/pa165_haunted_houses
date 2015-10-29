d260e494ddca852d564a4fd32ffc7f5c671e8b5a
Petr Skornok <skornok@gmail.com>
2015-10-29 16:26:25 +0100

    Basic Haunter Unit test

---
+++ src/test/java/com/peta2kuba/pa165_haunted_houses/dao/HaunterDaoTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.peta2kuba.pa165_haunted_houses.dao;
+
+import com.peta2kuba.pa165_haunted_houses.PersistenceTestAplicationContext;
+import com.peta2kuba.pa165_haunted_houses.entity.Haunter;
+import com.peta2kuba.pa165_haunted_houses.entity.House;
+import java.sql.Timestamp;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author skornok
+ */
+@ContextConfiguration(classes = PersistenceTestAplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class HaunterDaoTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private HaunterDao haunterDao;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    
+    public HaunterDaoTest() {
+    }
+
+     @Test
+     public void findAll() {
+         Haunter haunter = new Haunter();
+         
+         haunter.setDescription("More scary than seeing your naked grandmother!");
+         haunterDao.create(haunter);
+         
+         System.out.println(haunter);
+     }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
