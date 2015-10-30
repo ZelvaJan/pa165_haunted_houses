bcdd395ad009c5ffb9f0010d724d32bbf1684ff2
Jakub Turcovsky <turcovsky@master.cz>
2015-10-30 19:53:40 +0100

    unnecessary code removed

--- src/main/java/com/peta2kuba/pa165_haunted_houses/App.java
+++ src/main/java/com/peta2kuba/pa165_haunted_houses/App.java
@@ -1,14 +1,11 @@
 package com.peta2kuba.pa165_haunted_houses;
 
-import com.peta2kuba.pa165_haunted_houses.entity.Ability;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.List;
 
 /**
  * @author turcovsky on 28/10/15.
@@ -44,45 +41,6 @@ public class App {
 	}
 
 	private static void test() {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
 
-		//Haunter haunter = new Haunter();
-		//haunter.setId(1l);
-		//haunter.setName("Prema");
-		//haunter.setDescription("Haunting with his scary big head");
-		//
-		//HauntingHours hours = new HauntingHours();
-		//hours.setId(1l);
-		//hours.setFrom(new Time(System.currentTimeMillis()));
-		//hours.setTo(new Time(System.currentTimeMillis() + 50000));
-		//haunter.setHauntingHours(hours);
-
-		Ability ability = new Ability();
-		ability.setName("Big scary head");
-		ability.setDescription("It's really big!");
-
-		//List<Ability> abilityList = new ArrayList<>();
-		//abilityList.add(ability);
-		//haunter.setAbility(abilityList);
-
-		em.persist(ability);
-		em.getTransaction().commit();
-		em.close();
-
-		//Haunter result = em.createQuery(
-		//		"select haunter from Haunter haunter where haunter.id = 1", Haunter.class).getSingleResult();
-
-		em = emf.createEntityManager();
-		em.getTransaction().begin();
-		List<Ability> result = em.createQuery(
-				"select ability from Ability ability", Ability.class).getResultList();
-
-		em.close();
-
-		System.out.println("RESULT NAME = " + result.get(0).getName());
-		//System.out.println("Returned haunter:\n" +
-		//		"Haunter id = " + result.getId() +
-		//		"Haunter name = " + result.getName());
 	}
 }
