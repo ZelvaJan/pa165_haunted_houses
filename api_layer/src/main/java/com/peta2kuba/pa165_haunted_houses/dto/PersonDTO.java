0ac951afcbdf824b87ff2a01909260034b8087ea
DrakMelisek <petr.melicherik@seznam.cz>
2015-11-23 18:37:14 +0100

    PersonDTO

---
+++ api_layer/src/main/java/com/peta2kuba/pa165_haunted_houses/dto/PersonDTO.java
@@ -0,0 +1,68 @@
+package com.peta2kuba.pa165_haunted_houses.dto;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author petr.melicherik
+ */
+public class PersonDTO {
+
+    private Long id;
+    private String email;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.email);
+        hash = 53 * hash + Objects.hashCode(this.password);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PersonDTO other = (PersonDTO) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return true;
+    }
+}
