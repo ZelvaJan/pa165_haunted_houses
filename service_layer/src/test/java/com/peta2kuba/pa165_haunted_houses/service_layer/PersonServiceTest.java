989ee50b6b6a4c712af1402d99ec05391ef47919
Jakub Turcovsky <turcovsky@master.cz>
2015-11-26 17:26:18 +0100

    annotations in person test

--- service_layer/src/test/java/com/peta2kuba/pa165_haunted_houses/service_layer/PersonServiceTest.java
+++ service_layer/src/test/java/com/peta2kuba/pa165_haunted_houses/service_layer/PersonServiceTest.java
@@ -2,32 +2,40 @@ package com.peta2kuba.pa165_haunted_houses.service_layer;
 
 import com.peta2kuba.pa165_haunted_houses.dao.PersonDao;
 import com.peta2kuba.pa165_haunted_houses.entity.Person;
+import com.peta2kuba.pa165_haunted_houses.service_layer.config.ServiceConfiguration;
 import com.peta2kuba.pa165_haunted_houses.service_layer.service.PersonService;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
-import org.testng.annotations.BeforeMethod;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
 
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * @author turcovsky on 26/11/15.
  */
+@ContextConfiguration(classes=ServiceConfiguration.class)
 public class PersonServiceTest extends AbstractTransactionalTestNGSpringContextTests {
 
+	@Mock
 	private PersonDao personDao;
 
 	@Autowired
+	@InjectMocks
 	private PersonService personService;
 
 	private Person p1;
 	private Person p2;
+
+	@BeforeClass
+	public void setupClass() {
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@BeforeMethod
 	public void init() {
