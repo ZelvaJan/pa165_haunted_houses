fccaad4bc7116154ef66e1b851541c9f50c2c510
DrakMelisek <petr.melicherik@seznam.cz>
2015-11-26 17:55:12 +0100

    - tests

--- service_layer/src/test/java/com/peta2kuba/pa165_haunted_houses/service_layer/AbilityServiceTest.java
+++ service_layer/src/test/java/com/peta2kuba/pa165_haunted_houses/service_layer/AbilityServiceTest.java
@@ -1,7 +1,40 @@
 package com.peta2kuba.pa165_haunted_houses.service_layer;
 
+import com.peta2kuba.pa165_haunted_houses.PersistenceTestAplicationContext;
+import com.peta2kuba.pa165_haunted_houses.service_layer.config.ServiceConfiguration;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
+import static org.testng.Assert.fail;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 /**
- * @author turcovsky on 26/11/15.
+ * @author petr.melicherik
  */
-public class AbilityServiceTest {
+//@ContextConfiguration(classes=ServiceConfiguration.class)
+@ContextConfiguration(classes = PersistenceTestAplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+public class AbilityServiceTest extends AbstractTransactionalTestNGSpringContextTests {
+
+    @BeforeClass
+    public void setupClass() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @BeforeMethod
+    public void init() {
+
+    }
+    
+    @Test
+    public void editAbility() {
+        fail();
+    }
 }
---
+++ service_layer/src/test/java/com/peta2kuba/pa165_haunted_houses/service_layer/facade/AbilityFacadeImplTest.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.peta2kuba.pa165_haunted_houses.service_layer.facade;
+
+import com.peta2kuba.pa165_haunted_houses.dto.AbilityDTO;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author petr.melicherik
+ */
+public class AbilityFacadeImplTest {
+    
+//    public AbilityFacadeImplTest() {
+//    }
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+//
+//    /**
+//     * Test of create method, of class AbilityFacadeImpl.
+//     */
+//    @Test
+//    public void testCreate() {
+//        System.out.println("create");
+//        AbilityDTO ability = null;
+//        AbilityFacadeImpl instance = new AbilityFacadeImpl();
+//        instance.create(ability);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of edit method, of class AbilityFacadeImpl.
+//     */
+//    @Test
+//    public void testEdit() {
+//        System.out.println("edit");
+//        AbilityDTO ability = null;
+//        AbilityFacadeImpl instance = new AbilityFacadeImpl();
+//        instance.edit(ability);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of remove method, of class AbilityFacadeImpl.
+//     */
+//    @Test
+//    public void testRemove() {
+//        System.out.println("remove");
+//        AbilityDTO ability = null;
+//        AbilityFacadeImpl instance = new AbilityFacadeImpl();
+//        instance.remove(ability);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of findById method, of class AbilityFacadeImpl.
+//     */
+//    @Test
+//    public void testFindById() {
+//        System.out.println("findById");
+//        Long id = null;
+//        AbilityFacadeImpl instance = new AbilityFacadeImpl();
+//        AbilityDTO expResult = null;
+//        AbilityDTO result = instance.findById(id);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of findAll method, of class AbilityFacadeImpl.
+//     */
+//    @Test
+//    public void testFindAll() {
+//        System.out.println("findAll");
+//        AbilityFacadeImpl instance = new AbilityFacadeImpl();
+//        List<AbilityDTO> expResult = null;
+//        List<AbilityDTO> result = instance.findAll();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
