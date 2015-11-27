45b3fc5c2a0429f244dbc656595de2155bb53188
DrakMelisek <petr.melicherik@seznam.cz>
2015-11-27 15:24:48 +0100

    - SimpleIntegrationTest

--- service_layer/src/test/java/com/peta2kuba/pa165_haunted_houses/service_layer/facade/SimpleIntegrationTest.java
+++ service_layer/src/test/java/com/peta2kuba/pa165_haunted_houses/service_layer/facade/SimpleIntegrationTest.java
@@ -1,13 +1,14 @@
 package com.peta2kuba.pa165_haunted_houses.service_layer.facade;
 
-import com.peta2kuba.pa165_haunted_houses.dto.PersonDTO;
-import com.peta2kuba.pa165_haunted_houses.facade.PersonFacade;
+import com.peta2kuba.pa165_haunted_houses.dto.AbilityDTO;
+import com.peta2kuba.pa165_haunted_houses.facade.AbilityFacade;
 import com.peta2kuba.pa165_haunted_houses.service_layer.config.ServiceConfiguration;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
@@ -15,27 +16,26 @@ import org.testng.annotations.Test;
  * @author petr.melicherik
  */
 @ContextConfiguration(classes = ServiceConfiguration.class)
-public class SimpleIntegrationTest extends AbstractTransactionalTestNGSpringContextTests {
-    
+public class SimpleIntegrationTest extends AbstractTestNGSpringContextTests {
+
     @Autowired
-    private PersonFacade personFacade;
-    
-    PersonDTO personDTO;
+    AbilityFacade abilityFacade;
+
+    private final AbilityDTO ability = new AbilityDTO();
+
+    @BeforeClass
+    public void createData() {
+        ability.setName("Big Head");
+        abilityFacade.create(ability);
+    }
 
     /**
      * Test facade-database layers integration.
      */
     @Test
     public void simpleIntegrationTest() {
-        personDTO = new PersonDTO();
-        personDTO.setId((long) 51);
-        personDTO.setEmail("totaly new person");
-        personDTO.setPassword("passTopass");
-        
-        personFacade.createPerson(personDTO);
-        List<PersonDTO> list = personFacade.findAllPersons();
-        logger.error("Integration: " + list.toString());
+        List<AbilityDTO> list = abilityFacade.findAll();
         Assert.assertTrue(list != null && list.size() > 0);
     }
-    
+
 }
