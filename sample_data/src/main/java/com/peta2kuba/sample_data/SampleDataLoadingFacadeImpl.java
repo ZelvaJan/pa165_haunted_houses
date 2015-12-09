650f6851e312a93efa58280ba14739a425d5fdf3
Petr Skornok <skornok@gmail.com>
2015-12-09 20:56:08 +0100

    Sample Data

---
+++ sample_data/src/main/java/com/peta2kuba/sample_data/SampleDataLoadingFacadeImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.peta2kuba.sample_data;
+
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author skornok
+ */
+@Component
+@Transactional //transactions are handled on facade layer
+public class SampleDataLoadingFacadeImpl implements SampleDataLoadingFacade {
+    
+    final static Logger log = LoggerFactory.getLogger(SampleDataLoadingFacadeImpl.class);
+
+    @Override
+    @SuppressWarnings("unused")
+    public void loadData() throws IOException {
+        log.info("Loaded Haunted Houses.");
+    }
+    
+}
