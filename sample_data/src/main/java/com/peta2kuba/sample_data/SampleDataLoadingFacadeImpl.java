33a475b883c311666078b91ed1dab5870c6d583d
Jakub Turcovsky <jakubturcovsky@gmail.com>
2015-12-10 17:41:12 +0100

    sample data structure for multithread working

--- sample_data/src/main/java/com/peta2kuba/sample_data/SampleDataLoadingFacadeImpl.java
+++ sample_data/src/main/java/com/peta2kuba/sample_data/SampleDataLoadingFacadeImpl.java
@@ -25,6 +25,27 @@ public class SampleDataLoadingFacadeImpl implements SampleDataLoadingFacade {
     @SuppressWarnings("unused")
     public void loadData() throws IOException {
         log.info("Loaded Haunted Houses.");
+
+        createPeople();
+        createHouses();
+        createAbilities();
+        createHaunters();
+    }
+
+    private void createPeople() {
+
+    }
+
+    private void createHouses() {
+
+    }
+
+    private void createAbilities() {
+
+    }
+
+    private void createHaunters() {
+
     }
     
 }
