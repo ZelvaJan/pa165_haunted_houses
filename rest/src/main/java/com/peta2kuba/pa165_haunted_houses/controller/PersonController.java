e7e1b188f095f0b3ea434ffeedeffaaad1ebb6d4
Jakub Turcovsky <jakubturcovsky@gmail.com>
2015-12-18 20:14:40 +0100

    rest part done

--- rest/src/main/java/com/peta2kuba/pa165_haunted_houses/controller/PersonController.java
+++ rest/src/main/java/com/peta2kuba/pa165_haunted_houses/controller/PersonController.java
@@ -7,10 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.inject.Inject;
 import java.util.Collection;
@@ -27,6 +31,21 @@ public class PersonController {
 	@Inject
 	private PersonFacade personFacade;
 
+	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public final PersonDTO addPerson(@RequestBody PersonDTO person) throws Exception {
+
+		logger.debug("rest addPerson()");
+
+		personFacade.createPerson(person);
+		return person;
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public final void removePerson(@PathVariable("id") long id) throws Exception {
+		logger.debug("rest removePerson({})", id);
+
+		personFacade.removePersonById(id);
+	}
 
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public final Collection<PersonDTO> getPeople() throws JsonProcessingException {
@@ -40,17 +59,6 @@ public class PersonController {
 
 		logger.debug("rest getPersonById({})", id);
 		PersonDTO personDTO = personFacade.findPersonById(id);
-		if (personDTO == null){
-			throw new RuntimeException("Person not found!");
-		}
-		return personDTO;
-	}
-
-	@RequestMapping(value = "/{email}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public final PersonDTO getPersonByEmail(@PathVariable("email") String email) throws Exception {
-
-		logger.debug("rest getPersonByEmail({})", email);
-		PersonDTO personDTO = personFacade.findPersonByEmail(email);
 		if (personDTO == null){
 			throw new RuntimeException("Person not found!");
 		}
